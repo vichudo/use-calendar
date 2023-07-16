3d63d5824c8b69259f2dfb458837b690793bb91e
vichudo <vichudo@gmail.com>
2023-07-16 14:23:23 -0400

    add documentation

--- src/useCalendar.ts
+++ src/useCalendar.ts
@@ -1,29 +1,43 @@
 import { useState, useEffect } from 'react';
 import { format, startOfMonth, endOfMonth, eachDayOfInterval, addDays, subDays, isSameDay, getDay } from 'date-fns';
 
+/**
+ * @interface DateObject
+ * An interface to represent a date object, used for the dates in the current view scope of the calendar.
+ */
 interface DateObject {
-    date: string,
-    isCurrentMonth?: boolean,
-    isToday?: boolean,
-    isSelected?: boolean
+    date: string, // Date in 'yyyy-MM-dd' format
+    isCurrentMonth?: boolean, // Flag to indicate if the date is in the current month
+    isToday?: boolean, // Flag to indicate if the date is the current day
+    isSelected?: boolean // Flag to indicate if the date is the selected date
 }
 
+/**
+ * useCalendar is a custom React hook that provides functionality for managing a calendar view.
+ * It generates an array of DateObjects to represent the dates in the current month,
+ * including padding dates from the previous and next month.
+ * It also provides functionality for selecting a date and navigating through months.
+ */
 const useCalendar = () => {
-    const [today, setToday] = useState(new Date());
-    const [currentMonth, setCurrentMonth] = useState(new Date());
-    const [selectedDate, setSelectedDate] = useState(today);
-    const [currentScope, setCurrentScope] = useState<DateObject[]>([]);
+    const [today, setToday] = useState(new Date()); // The current date
+    const [currentMonth, setCurrentMonth] = useState(new Date()); // The current month being displayed
+    const [selectedDate, setSelectedDate] = useState(today); // The currently selected date
+    const [currentScope, setCurrentScope] = useState<DateObject[]>([]); // The current scope of dates to display in the calendar view
 
     useEffect(() => {
-        generateDates();
+        generateDates(); // Generate the dates whenever the current month or selected date changes
     }, [currentMonth, selectedDate]);
 
+    /**
+     * Generates the dates for the calendar view, including padding dates from the previous and next month.
+     */
     const generateDates = () => {
         const start = startOfMonth(currentMonth);
         const end = endOfMonth(currentMonth);
 
-        const startDayOfWeek = (getDay(start) + 6) % 7; // Shift Sunday to index 6 (end of the week)
-        const endDayOfWeek = (6 - ((getDay(end) + 6) % 7)); // Calculate the padding after end of the month.
+        // Adjusts the week start to Monday
+        const startDayOfWeek = (getDay(start) + 6) % 7;
+        const endDayOfWeek = (6 - ((getDay(end) + 6) % 7));
 
         const daysBeforeStart = subDays(start, startDayOfWeek);
         const daysAfterEnd = addDays(end, endDayOfWeek);
@@ -46,10 +60,18 @@ const useCalendar = () => {
         setCurrentScope(dates);
     }
 
+    /**
+     * Sets the selected date.
+     * @param {Date} date - The date to be selected.
+     */
     const selectDate = (date: Date) => {
         setSelectedDate(date);
     }
 
+    /**
+     * Changes the current month by a given offset.
+     * @param {number} offset - The number of months to add to the current month. Can be negative to go back.
+     */
     const navigateMonth = (offset: number) => {
         setCurrentMonth((prevMonth: Date) => {
             const newMonth = new Date(prevMonth.getFullYear(), prevMonth.getMonth() + offset, 1);
@@ -57,6 +79,7 @@ const useCalendar = () => {
         });
     };
 
+    // Return the current date, dates to display, selected date, current month, and functions to select a date and navigate through months
     return { today, currentScope, selectedDate, currentMonth, selectDate, navigateMonth };
 }
 
